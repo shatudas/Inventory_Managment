188032b43312e4e465ae4e5b80bc6ae436f7cfa6
shatudas <das.shatu2000@gmail.com>
2023-04-25 12:02:21 +0600

    Laravel routing controller

--- resources/views/backend/layouts/sideber.blade.php
+++ resources/views/backend/layouts/sideber.blade.php
@@ -1,9 +1,13 @@
-    
+@php
+ $prefix = Request::route()->getPrefix();
+ $route = Route::current()->getName();
+@endphp
+  
  <!-- Sidebar Menu -->
  <nav class="mt-2">
   <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-   
-   <li class="nav-item">
+
+   <li class="nav-item {{($prefix=='/user')?'menu-open':''}}">
     <a href="#" class="nav-link">
      <i class="nav-icon fas fa-copy"></i>
      <p>
@@ -11,9 +15,10 @@
       <i class="fas fa-angle-left right"></i>
      </p>
     </a>
+
     <ul class="nav nav-treeview">
      <li class="nav-item">
-      <a href="#" class="nav-link">
+      <a href="{{ route('user.view') }}" class="nav-link {{($route=='user.view')?'active':''}}">
        <i class="far fa-circle nav-icon"></i>
        <p> View User </p>
       </a>
--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('auth/login');
+  return view('auth/login');
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+//-------user---------//
+Route::prefix('user')->group(function()
+{
+	Route::get('/view','Backend\UserController@view')->name('user.view');
+	Route::get('/add','Backend\UserController@add')->name('user.add');
+	Route::post('/store','Backend\UserController@store')->name('user.store');
+	Route::get('/edit/{id}','Backend\UserController@edit')->name('user.edit');
+	Route::post('/update/{id}','Backend\UserController@update')->name('user.update');
+	Route::get('/delete/{id}','Backend\UserController@delete')->name('user.delete');
+	Route::get('/active/{id}','Backend\UserController@active')->name('user.active');
+	Route::get('/inactive/{id}','Backend\UserController@inactive')->name('user.inactive');
+});
+
